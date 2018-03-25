a1272dd166ad248032e18d4190b067897392911b
uhey22e <uhey22e@users.noreply.github.com>
2018-03-26 00:59:34 +0900

    Add S3 bucket and CloudFront for frontend

--- client/src/api/weightLog.js
+++ client/src/api/weightLog.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default {
   get: () => {
-    const url = 'https://6mcdo0mnr8.execute-api.ap-northeast-1.amazonaws.com/dev/weight_log';
+    const url = 'https://b55huow1uf.execute-api.ap-northeast-1.amazonaws.com/dev/weight_log';
     return axios.get(url)
       .then(res => res.data);
   },
